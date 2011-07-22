53261c565d3e947c19991819993a7521a1c431cc
Contra <contra@maricopa.edu>
2011-07-22 15:32:44 -0700

    reimplimented xss, implemented sqli, fixes everywhere

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ fusker.config.verbose = true; //Verbose logging. Default: false
 //Configure detections, loaded from detectives folder. 
 //Tests will be executed in order, always put 404 last if you choose to use it
 fusker.detectives.push('xss'); //Checks URL for common XSS
+fusker.detectives.push('sqli'); //Checks URL for common SQL queries
 fusker.detectives.push('csrf'); //Checks headers for CSRF attacks
 fusker.detectives.push('lfi'); //Checks URL for common LFI
 fusker.detectives.push('404'); //Only use for single page applications and if you want to punish snoopers
